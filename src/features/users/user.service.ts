5482f1cda6fe9c7b6fa8891635230e02ce20e08a
LoaiMasri <s12010098@stu.najah.edu>
2022-03-09 21:46:30 +0200

    trying to send email

--- src/features/users/user.service.ts
+++ src/features/users/user.service.ts
@@ -4,12 +4,15 @@ import { Repository } from 'typeorm';
 import { UserEntity } from './entities/user.entity';
 import { UserModel } from './models/user.model';
 import * as bcrypt from 'bcrypt';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class UsersService {
   constructor(
+
     @InjectRepository(UserEntity)
     private _userRepository: Repository<UserEntity>,
+    private _mailerService: MailerService,
   ) {}
 
   async findOne(username: string): Promise<UserEntity | undefined> {
@@ -19,6 +22,7 @@ export class UsersService {
   async create(model: UserModel): Promise<any> {
     let exist=await this._userRepository.findOne({where:{email:model.email}});
     if(exist) return { status:false, message:"Email already exist" };
+    await this.sendMail(model.email);
     let salt=await bcrypt.genSalt(10);
     let entity={
         email:model.email,
@@ -32,4 +36,19 @@ export class UsersService {
     return this._userRepository.find();
   }
 
+  async sendMail(email: string) {
+    await this._mailerService.sendMail({
+      to: email,
+      from:process.env.EMAIL_USER,
+      subject: 'Hello from Nest JS ✔',
+      text:'this is a test mail',
+    }).then(() => {
+      console.log('Email sent!');
+    }).catch((err) => {
+      console.log('Error sending email: ', err);
+    });
+  }
+
 }
+
+
